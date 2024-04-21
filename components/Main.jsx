aefd7e6eb56549d54b947972f9e52089eee6f926
Anezz12 <harsenaargrtya1@gmail.com>
2024-04-21 08:13:45 +0700

    update

--- components/Main.jsx
+++ components/Main.jsx
@@ -267,8 +267,8 @@ function Main() {
                 <path className="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
               </svg>
               <blockquote>
-                <p className="text-2xl italic font-medium text-gray-900 dark:text-white">
-                  "The World is full of kind people... If you can't find one, be
+                <p className="text-xl italic font-medium text-gray-900 dark:text-white">
+                  "The World is full of kind people...If you can't find one, be
                   one.."
                 </p>
               </blockquote>
